1ec83ecd5a950363c8698d21f611311a85e55f4b
Curbe8 <l_curbe@hotmail.com>
2019-09-11 19:13:37 -0300

    Parte 3b
    Implementacion de la interfaz de acceso a datos mediante MongoDB.

--- DataAccessLayer/DALEmployeesMongo.cs
+++ DataAccessLayer/DALEmployeesMongo.cs
@@ -1,37 +1,85 @@
-﻿using Shared.Entities;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using Shared.Entities;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccessLayer
 {
     public class DALEmployeesMongo : IDALEmployees
     {
-        public void AddEmployee(Employee emp)
+        private static MongoClient client = new MongoClient("mongodb://localhost:27017");
+        private static IMongoDatabase database = client.GetDatabase("foo");
+        private IMongoCollection<BsonDocument> collection = database.GetCollection<BsonDocument>("bar");
+
+        public async void AddEmployee(Employee emp)
         {
-            throw new NotImplementedException();
+            BsonDocument datos = new BsonDocument();
+            datos.Add("ID", emp.Id);
+            datos.Add("NAME", emp.Name);
+            datos.Add("START_DATE", emp.StartDate);
+
+            if (emp.GetType() == typeof(FullTimeEmployee))
+            {
+                FullTimeEmployee fullTime = (FullTimeEmployee)emp;
+                datos.Add("SALARY", fullTime.Salary);
+            }
+            else
+            {
+                PartTimeEmployee partTime = (PartTimeEmployee)emp;
+                datos.Add("RATE", partTime.HourlyRate);
+            }
+            await this.collection.InsertOneAsync(datos);
         }
 
         public void DeleteEmployee(int id)
         {
-            throw new NotImplementedException();
+            BsonDocument filter = new BsonDocument();
+            filter.Add("ID", id);
+            this.collection.DeleteOne(filter);
         }
 
         public void UpdateEmployee(Employee emp)
         {
-            throw new NotImplementedException();
+            BsonDocument filter = new BsonDocument();
+            filter.Add("ID", emp.Id);
+            BsonDocument datos = new BsonDocument();
+            datos.Add("ID", emp.Id);
+            datos.Add("NAME", emp.Name);
+            datos.Add("START_DATE", emp.StartDate);
+
+            if (emp.GetType() == typeof(FullTimeEmployee))
+            {
+                FullTimeEmployee fullTime = (FullTimeEmployee)emp;
+                datos.Add("SALARY", fullTime.Salary);
+            }
+            else
+            {
+                PartTimeEmployee partTime = (PartTimeEmployee)emp;
+                datos.Add("RATE", partTime.HourlyRate);
+            }
+            this.collection.UpdateOne(filter, datos);
         }
 
         public List<Employee> GetAllEmployees()
         {
-            throw new NotImplementedException();
+            return new List<Employee>();
         }
 
         public Employee GetEmployee(int id)
         {
-            throw new NotImplementedException();
+            BsonDocument filter = new BsonDocument();
+            filter.Add("ID", id);
+            BsonDocument documento = (BsonDocument)this.collection.Find(filter);
+            IEnumerator<BsonValue> it = documento.Values.GetEnumerator();
+            while(it.Current != null)
+            {
+                BsonValue valor = it.Current;
+                valor.ToString();
+                it.MoveNext();
+
+            };
+            return new FullTimeEmployee();
         }
     }
 }
